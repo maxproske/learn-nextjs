fd986c16a500bbbc65bf0afd216eb957148769ca
Max <max@mproske.com>
2020-08-14 18:22:04 -0700

    ThemeUI sucks

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Link  from 'next/link'
+import Link from 'next/link'
 
 const Page = () => {
-    return (
-        <div>
-        <h1>Hello, world!</h1>
-        <Link href="/notes">
-            <a>Notes</a>
-        </Link>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+    </div>
+  )
 }
 
 export default Page
---
+++ src/theme.js
@@ -0,0 +1,19 @@
+import { roboto } from '@theme-ui/presets'
+
+const theme = {
+  ...roboto,
+  colors: {
+    ...roboto,
+    background: 'grey',
+  },
+  containers: {
+    page: {
+      width: '100%',
+    },
+  },
+  styles: {
+    ...roboto.styles,
+  },
+}
+
+export default theme
